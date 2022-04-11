f9e6f5928f8e7b67e51c4f90af66406185992d15
vaddddie <v_bykovski@mail.ru>
2022-04-11 13:09:52 +0300

    initial commit

---
+++ djangotest/main/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Status(models.Model):
+    FarmName = models.CharField('Farm name:', max_length=50)
+    Temperature = models.FloatField('Temperature:')
+    Humidity = models.IntegerField('Humidity:')
+    Light = models.IntegerField('Light:')
+    TimeLeft = models.DateTimeField('Time Left:')
+    GrowthProcess = models.IntegerField('Growth Process:')
+
+    def __str__(self):
+        return self.FarmName
---
+++ djangotest/main/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from .models import Status
+
+
+def index(request):
+    statuses = Status.objects.all()
+    return render(request, 'main/index.html', {'statuses': statuses})
+
+
+def management(request):
+    statuses = Status.objects.all()
+    if request.POST.get('TempSet'):
+        print("Кнопка сет")
+    if request.POST.get('LightOn'):
+        print("СЮДАААА ЛУУУУТ")
+    if request.POST.get('LightOff'):
+        print("СЮДАААА ЛУУУУТ")
+    if request.POST.get('WateringOn'):
+        print("СЮДАААА ЛУУУУТ")
+    if request.POST.get('WateringOff'):
+        print("СЮДАААА ЛУУУУТ")
+    if request.POST.get('Default'):
+        print("СЮДАААА ЛУУУУТ")
+    return render(request, 'main/management.html', {'statuses': statuses})
+
+
+def mode(request):
+    return render(request, 'main/mode.html')
