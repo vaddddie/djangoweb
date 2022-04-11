2c48210c3e3555edab65dea68079a6502a90c457
vaddddie <v_bykovski@mail.ru>
2022-04-11 14:38:45 +0300

    mqtt added

--- djangotest/main/__init__.py
+++ djangotest/main/__init__.py
@@ -7,21 +7,26 @@ topic = "test/blink"
 client_id = f'python-mqtt-{0}'
 
 
-def connect_mqtt(broker, port, topic, client_id):
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
-    # Set Connecting Client ID
-    client = mqtt_client.Client(client_id)
-    #client.username_pw_set(username, password)
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
+class connect():
+    def __init__(self):
+        connect.self = self
 
-def client():
-    return client
+    def connect_mqtt(broker, port, topic, client_id):
+        def on_connect(client, userdata, flags, rc):
+            if rc == 0:
+                print("Connected to MQTT Broker!")
+            else:
+                print("Failed to connect, return code %d\n", rc)
+        # Set Connecting Client ID
+        client = mqtt_client.Client(client_id)
+        #client.username_pw_set(username, password)
+        client.on_connect = on_connect
+        client.connect(broker, port)
+        return client
 
-client = connect_mqtt(broker, port, topic, client_id)
+    def client(self):
+        return client
+
+
+client = connect.connect_mqtt(broker, port, topic, client_id)
 client.loop_start()
--- djangotest/main/mqttoutput.py
+++ djangotest/main/mqttoutput.py
@@ -1,7 +1,11 @@
-import __init__
+from .__init__ import connect
 
 
 class blink:
+    def __init__(self):
+        blink.self = self
+
+
     def blinkOn():
         client = __init__.client()
         client.publish(topic, 1)
