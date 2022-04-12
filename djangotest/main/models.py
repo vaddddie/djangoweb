3b19b4fe660299f6d003d0fe5925c5a660ef6dab
vaddddie <v_bykovski@mail.ru>
2022-04-12 15:28:08 +0300

    buf fixed

--- djangotest/main/models.py
+++ djangotest/main/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Status(models.Model):
-    FarmName = models.CharField('Farm name:', max_length=50)
-    Temperature = models.FloatField('Temperature:',blank=True)
+    FarmName = models.CharField('Farm name:', max_length=50, blank=True)
+    Temperature = models.FloatField('Temperature:')
     Humidity = models.IntegerField('Humidity:')
     Light = models.IntegerField('Light:')
     TimeLeft = models.DateTimeField('Time Left:')
