b09d43323a3f9f7116609ce6a39c7ed8a3762461
vaddddie <v_bykovski@mail.ru>
2022-04-18 15:36:06 +0300

    bug fixed

--- djangotest/main/mqttController.py
+++ djangotest/main/mqttController.py
@@ -1,12 +1,14 @@
 from paho.mqtt import client as mqtt_client
 
-
+"""
 broker = '192.168.4.1'
 port = 1883
 topic = "test/blink"
 client_id = f'python-mqtt-{0}'
 username = 'emqx'
 password = 'public'
+"""
+
 
 
 def connect_mqtt(broker, port, topic, client_id):
@@ -15,11 +17,16 @@ def connect_mqtt(broker, port, topic, client_id):
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)
+        client.subscribe("test/heartbeat")
+
+    def on_message(client, userdata, msg):
+        print(msg.payload)
 
     # Set Connecting Client ID
     client = mqtt_client.Client(client_id)
     # client.username_pw_set(username, password)
     client.on_connect = on_connect
+    client.on_message = on_message
     client.connect(broker, port)
     return client
 
